b5b48137f9848cd75c1e51d55a62d0ed8d66bc76
Pawel <pawelbabiak@gmail.com>
2026-07-25 23:49:57 +0100

    working with classes

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     clock = pygame.time.Clock()
+    clap = pygame.mixer.Sound('samples\clap-808.wav')
     
     running = True
     while running:
@@ -19,11 +20,16 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+                elif event.key == pygame.K_SPACE:
+                    clap.play()
                     
+        
         screen.fill(bg)
         
         ### Render ###
-        pygame.draw.circle(screen,palette['colors'][0], (width/2, height/2), 10)
+        spam = Test()
+        spam.draw(screen)
+        # pygame.draw.circle(screen,palette['colors'][0], (width/2, height/2), 10)
         pygame.draw.circle(screen,palette['colors'][1], (width/2, height/2 + 20), 10)
         pygame.draw.circle(screen,palette['colors'][2], (width/2, height/2 + 40), 10)
         pygame.draw.circle(screen,palette['colors'][3], (width/2, height/2 + 60), 10)
@@ -33,6 +39,33 @@ def main():
         clock.tick(60)
         ###
     pygame.quit()
+
+class Test:
+    print('Im a class')
+    def draw(self, screen):
+        pygame.draw.circle(screen,palette['colors'][0], (width/2, height/2), 10)
+        
+    # def __init__(self, radius, color, surface, pos):
+    #     self.radius = radius
+    #     self.color = color
+    #     self.surface = surface
+    #     self.pos = pos
+    
+    # def (self):
+    #     pass
+
+
+class instrument:
+    def __init__(self, radius, sound, color, position, key, surface):
+        self.radius = radius
+        self.sound = sound
+        self.color = color
+        self.position = position
+        self.key = key
+        self.surface = surface
+        
+    def draw(self):
+        pygame.draw.circle(self.surface,)
     
     
 if __name__ == '__main__':
