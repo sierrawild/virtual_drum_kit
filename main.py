be3fc601fb008fb4f1977eb7dbc2a38ab17896f3
Pawel <pawelbabiak@gmail.com>
2026-07-28 23:22:37 +0100

    Extending event handling

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, random, palettes
+import pygame, random, palettes, math
 
 def main():
     pygame.mixer.pre_init(44100, -16, 2, 512)
@@ -28,8 +28,9 @@ def main():
               }
     
     # instruments initialization
-    clap = Instrument(sounds['clap'])
-    snare = Instrument(sounds['snare'])
+    snare = Instrument(sounds['snare'], surface=screen, color=palette['colors'][0], position=(width/2, height*0.8), radius=50, key=pygame.K_b)
+    
+    instruments = [snare]
     
     running = True
     while running:
@@ -40,22 +41,18 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
-                elif event.key == pygame.K_SPACE:
-                    clap.play()
-                elif event.key == pygame.K_s:
-                    snare.play()
-                    
-                    
+            
+            for instrument in instruments:
+                instrument.keybord_input(event)
         
+
+                
         screen.fill(bg)
         
         ### Render ###
-        snare.draw(screen, palette['colors'][0], (100,100), 50)
-        pygame.draw.circle(screen,palette['colors'][0], (width/2, height/2), 10)
-        pygame.draw.circle(screen,palette['colors'][1], (width/2, height/2 + 20), 10)
-        pygame.draw.circle(screen,palette['colors'][2], (width/2, height/2 + 40), 10)
-        pygame.draw.circle(screen,palette['colors'][3], (width/2, height/2 + 60), 10)
-        pygame.draw.circle(screen,palette['colors'][4], (width/2, height/2 + 80), 10)
+        for instrument in instruments:
+            instrument.run()
+
         
         pygame.display.flip()
         clock.tick(60)
@@ -64,28 +61,53 @@ def main():
 
 
 class Instrument:
-    def __init__(self, sound):
+    def __init__(self, sound, surface, color, position, radius, key, weights=[6,1]):
+        
+        self.weights = weights
+        self.surface = surface
+        self.color = color
+        self.position = position
+        self.radius = radius
+        self.key = key
+        
         # sets the sound1 and checks if the library has second sound. 
-        # If it dosnt both sounds will be the same
+        # If it dosnt esist both sounds will be the same
         self.sound1 = pygame.mixer.Sound(sound[0])
         try:
             self.sound2 = pygame.mixer.Sound(sound[1])
         except IndexError:
             self.sound2 = pygame.mixer.Sound(sound[0])
         
-    def draw(self, surface, color, position, radius):
-        p = pygame.Vector2(position)
-        pygame.draw.circle(surface, color, p, radius)
         
-    def play(self, weights=[6,1]):
+    def draw(self):
+        p = pygame.Vector2(self.position)
+        pygame.draw.circle(self.surface, self.color, p, self.radius)
+        
+    def play(self):
         '''Plays the sounds. Adjust the weights to set how oftern 2 diferent sounds play'''
-        choice = random.choices([1,2], weights=weights, k= 1)
+        choice = random.choices([1,2], weights=self.weights, k= 1)
         if choice[0] == 1:
             self.sound1.play()
         else:
             self.sound2.play()
-            
+    
+    def mouse(self):
+        mouse_x = pygame.mouse.get_pos()[0]
+        mouse_y = pygame.mouse.get_pos()[1]
         
+        sqx = (mouse_x -self.position[0])**2
+        sqy = (mouse_y -self.position[1])**2
+                    
+        if pygame.mouse.get_just_pressed()[0] and math.sqrt(sqx + sqy) < self.radius:         
+            self.play()
+    
+    def keybord_input(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == self.key:
+                self.play()
+    def run(self):
+        self.draw()
+        self.mouse()
     
     
 if __name__ == '__main__':
